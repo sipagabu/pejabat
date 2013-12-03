d56a07efb1a3257998e2b54c95bfa41b52420ba4
Andareas Silaban <andareas@gmail.com>
2013-12-03 18:05:56 +0700

    Create table_pejabat.sql

---
+++ table_pejabat.sql
@@ -0,0 +1,12 @@
+create table pejabat_table (
+No INT NOT NULL AUTO_INCREMENT, 
+ Nama  varchar(100) not null, 
+ Departemen  varchar(100) not null, 
+ Handphone  varchar(100) not null, 
+ Email  varchar(100) not null, 
+ Telephone_Kantor  varchar(100) not null, 
+ Ajudan  varchar(100) not null, 
+ Handphone_Ajudan  varchar(100) not null, 
+ Email_Ajudan  varchar(100) not null,
+PRIMARY KEY ( No )
+);
