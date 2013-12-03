ac6d31ba6fc5506dde20c66b9ae6bb56bf0c4166
Andareas Silaban <andareas@gmail.com>
2013-12-03 18:25:57 +0700

    Update table_pejabat.sql

--- table_pejabat.sql
+++ table_pejabat.sql
@@ -1,12 +1,16 @@
 create table pejabat_table (
 No INT NOT NULL AUTO_INCREMENT, 
  Nama  varchar(100) not null, 
- Departemen  varchar(100) not null, 
- Handphone  varchar(100) not null, 
- Email  varchar(100) not null, 
- Telephone_Kantor  varchar(100) not null, 
- Ajudan  varchar(100) not null, 
- Handphone_Ajudan  varchar(100) not null, 
- Email_Ajudan  varchar(100) not null,
+ Departemen  varchar(100), 
+ Handphone  varchar(100), 
+ Email  varchar(100), 
+ Telephone_Kantor  varchar(100), 
+ Ajudan  varchar(100), 
+ Handphone_Ajudan  varchar(100), 
+ Email_Ajudan  varchar(100),
 PRIMARY KEY ( No )
-);
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+
+
+insert into 'pejabat_table' ('No','Nama','Departemen','Handphone','Email','Telephone_Kantor','Ajudan','Handphone_Ajudan','Email_Ajudan')  values (1,"Susilo Bambang Yudhoyono","Presiden RI","088888888","sby@presiden.info","Telephone Kantor","Ajudan","Handphone Ajudan","Email Ajudan");
