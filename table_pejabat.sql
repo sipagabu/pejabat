1b4304cdb594f96ebad14882f657cf76ac716d86
Andareas Silaban <andareas@gmail.com>
2013-12-05 07:02:52 +0700

    Update table_pejabat.sql

--- table_pejabat.sql
+++ table_pejabat.sql
@@ -1,16 +1,18 @@
 create table pejabat_table (
-No INT NOT NULL AUTO_INCREMENT, 
- Nama  varchar(100) not null, 
- Departemen  varchar(100), 
- Handphone  varchar(100), 
- Email  varchar(100), 
- Telephone_Kantor  varchar(100), 
- Ajudan  varchar(100), 
- Handphone_Ajudan  varchar(100), 
- Email_Ajudan  varchar(100),
-PRIMARY KEY ( No )
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+ID int NOT NULL AUTO_INCREMENT, 
+Nama  varchar(100) not null, 
+Departemen  varchar(100), 
+Handphone  varchar(100), 
+Email  varchar(100), 
+Telephone_Kantor  varchar(100), 
+Ajudan  varchar(100), 
+Handphone_Ajudan  varchar(100), 
+Email_Ajudan  varchar(100),
+PRIMARY KEY ( ID )
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=6363 ;
 
 
 
-insert into 'pejabat_table' ('No','Nama','Departemen','Handphone','Email','Telephone_Kantor','Ajudan','Handphone_Ajudan','Email_Ajudan')  values (1,"Susilo Bambang Yudhoyono","Presiden RI","088888888","sby@presiden.info","Telephone Kantor","Ajudan","Handphone Ajudan","Email Ajudan");
+INSERT INTO  `pejabat_table` (`ID`, `Nama`, `Departemen`, `Handphone`, `Email`, `Telephone_Kantor`, `Ajudan`, `Handphone_Ajudan`, `Email_Ajudan`) 
+VALUES 
+(NULL, 'SBY', 'Presiden ', '08888888', 'sby@presideninfo.bl', '021342123', 'aldrin pasha', '09999999', 'aldpas@gmail.com');
