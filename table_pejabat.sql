93d294c533224006df148b39974f4506d86d1e5e
Andareas Silaban <andareas@gmail.com>
2013-12-06 20:42:24 +0700

    Update table_pejabat.sql

--- table_pejabat.sql
+++ table_pejabat.sql
@@ -1,7 +1,8 @@
 create table pejabat_table (
 ID int NOT NULL AUTO_INCREMENT, 
-Nama  varchar(100) not null, 
-Departemen  varchar(100), 
+Nama  varchar(100), 
+Departemen  varchar(100),
+Jabatan varchar(100), 
 Handphone  varchar(100), 
 Email  varchar(100), 
 Telephone_Kantor  varchar(100), 
@@ -13,6 +14,28 @@ PRIMARY KEY ( ID )
 
 
 
-INSERT INTO  `pejabat_table` (`ID`, `Nama`, `Departemen`, `Handphone`, `Email`, `Telephone_Kantor`, `Ajudan`, `Handphone_Ajudan`, `Email_Ajudan`) 
-VALUES 
-(NULL, 'SBY', 'Presiden ', '08888888', 'sby@presideninfo.bl', '021342123', 'aldrin pasha', '09999999', 'aldpas@gmail.com');
+
+
+
+INSERT INTO `pejabat`.`pejabat_table` (`ID`, `Nama`, `Departemen`, `Jabatan`, `Handphone`, `Email`, `Telephone_Kantor`, `Ajudan`, `Handphone_Ajudan`, `Email_Ajudan`) VALUES (NULL ,  'M. Chatib Basri',  'Kementerian Keuangan',  'Menteri Keuangan R.I.', NULL , NULL ,  'Telp. (021) 381.4324 Ext. 6000, 6100 Fax. (021) 350.0842', NULL , NULL , NULL),
+(NULL, 'Anny Ratnawati', 'Kementerian Keuangan','Wakil Menteri Keuangan I.', NULL, NULL, 'Telp. (021) 350.5605 Ext. 6200 Fax. (021) 350.5650', NULL , NULL , NULL),
+(NULL, 'Bambang P.S Brodjonegoro', 'Kementerian Keuangan','Wakil Menteri Keuangan II.', NULL, NULL, 'Telp. (021) 350.3931 Ext. 6201 Fax. (021) 381.2328', NULL, NULL, NULL),
+(NULL, 'Kiagus Ahmad Badaruddin', 'Kementerian Keuangan','Sekretaris Jenderal', NULL, NULL, 'Telp. (021) 384.0651, 351.0055 Ext. 6116, 6117, 6118 Fax. (021) 345.3710, 385.2143, 384.4784', NULL, NULL, NULL),
+(NULL, 'Askolani','Kementerian Keuangan', 'Plt. Direktur Jenderal Anggaran.', NULL, NULL, 'Telp. (021) 384.9315 Ext. 8176 Fax. (021) 384.7157', NULL, NULL, NULL),
+(NULL, 'A. Fuad Rahmany','Kementerian Keuangan', 'Direktur Jendral Pajak', NULL, NULL, 'Telp. (021)  529.60791 Fax.  (021)  520.3184', NULL, NULL, NULL),
+(NULL, 'Agung Kuswandono','Kementerian Keuangan', 'Direktur Jenderal Bea dan Cukai', NULL, NULL, 'Telp.  (021)  489.7511 Fax.   (021)  489.7512', NULL, NULL, NULL),
+(NULL, 'Boediarso Teguh Widodo','Kementerian Keuangan', 'Plt. Direktur Jenderal Perbendaharaan', NULL, NULL, 'Telp.  (021) 345.0959 Ext. 5200 Fax.   (021) 345.7490', NULL, NULL, NULL),
+(NULL, 'Hadiyanto','Kementerian Keuangan', 'Direktur Jenderal Kekayaan Negara', NULL, NULL, ' Telp.  (021)  384.9388 Fax.   (021)  344.8390', NULL, NULL, NULL),
+(NULL, 'Marwanto Harjowirjono','Kementerian Keuangan', 'Direktur Jenderal Perimbangan Keuangan', NULL, NULL, 'Telp. (021) 350.9442 Ext. 7563 Fax.  (021) 350.9443', NULL, NULL, NULL),
+(NULL, 'Robert Pakpahan','Kementerian Keuangan', 'Kuasa Khusus Direktur Jenderal Pengelolaan Utang', NULL, NULL, 'Telp.  (021)  350.0841 Fax. (021)  351.2095', NULL, NULL, NULL),
+(NULL, 'Vincentius Sonny Loho', 'Kementerian Keuangan','Inspektur  Jenderal', NULL, NULL, 'Telp.  (021)345.6546 Ext. 6555 Fax.   (021)  384.7442', NULL, NULL, NULL),
+(NULL, NULL, 'Kementerian Keuangan','Kepala Badan Kebijakan Fiskal', NULL, NULL, 'Telp.  (021)  344.1484 Ext. 7030, 7031 Fax.   (021)   384.8049', NULL, NULL, NULL),
+(NULL, NULL, 'Kementerian Keuangan','Kepala Badan Pendidikan Pelatihan Keuangan', NULL, NULL, 'Telp.  (021)724.4873  (021)739.4666 ext. 251 Fax.(021)724.4912', NULL, NULL, NULL),
+(NULL, NULL, 'Kementerian Keuangan','Staf Ahli Menteri Keuangan Bidang Kekayaan Negara', NULL, NULL, 'TGd. “E” (Gd. Djuanda I) Lt. 7 Ext. 6211 Fax.   (021) 384.9176', NULL, NULL, NULL),
+(NULL, 'Andin Hadiyanto','Kementerian Keuangan', 'Staf Ahli Menteri Keuangan Bidang Makro Ekonomi dan Keuangan Internasional', NULL, NULL, ' Telp.  (021) 384.9176 Fax.   (021) 384.7465', NULL, NULL, NULL),
+(NULL, NULL,'Kementerian Keuangan', 'Staf Ahli Menteri Keuangan Bidang Kebijakan dan Regulasi Jasa Keuangan dan Pasar Modal', NULL, NULL, ' T. (021) 384.9176 F. (021) 384.7465', NULL, NULL, NULL),
+(NULL, 'Robert Pakpahan','Kementerian Keuangan', 'Staf Ahli Menteri Keuangan Bidang Penerimaan Negara', NULL, NULL, 'T. (021) 384.9176 F. (021) 384.7465', NULL, NULL, NULL),
+(NULL, 'Boediarso Teguh Widodo','Kementerian Keuangan', 'Staf Ahli Menteri Keuangan Bidang Pengeluaran Negara', NULL, NULL, 'T. (021) 384.9176 Ext. 6211, 6222 F. (021) 384.7465', NULL, NULL, NULL),
+(NULL, 'Rionald Silaban','Kementerian Keuangan', 'Staf Ahli Bidang Organisasi, Birokrasi, dan Teknologi Informasi', NULL, NULL, ' T. (021) 384.9176 Ext. 6211 F. (021) 384.7465', NULL, NULL, NULL);
+
+
